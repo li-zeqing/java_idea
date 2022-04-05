02c95fdfc654612bece75c5317d0b3bedc917ab7
lizeqing <lizeqing77@163.com>
2022-04-05 10:01:40 +0800

    my Thread test

---
+++ JavaSenior/src/com/mylearn/java/ThreadTest3.java
@@ -0,0 +1,12 @@
+package com.mylearn.java;
+
+/**
+ * @author Zeqing Li Email:lizeqing77@163.com
+ * @Description
+ * @date 2022-04-05 9:59
+ */
+public class ThreadTest3 {
+    public static void main(String[] args) {
+        System.out.println("This is my test!");
+    }
+}
