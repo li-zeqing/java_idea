c13762da4fb6980bfd4808d251ee153814ee0a4f
lizeqing <lizeqing77@163.com>
2022-04-07 22:23:44 +0800

    Runnable test update

--- JavaSenior/src/com/mylearn/java/ThreadTest2.java
+++ JavaSenior/src/com/mylearn/java/ThreadTest2.java
@@ -7,7 +7,7 @@ package com.mylearn.java;
  * 1.创建一个实现了Runnable接口的类
  * 2.实现类去实现Runnable中的抽象方法Run()
  * 3.创建实现类的对象
- * 4.将此对象作为参数传递到THread类的构造器中，创建Thread类的对象
+ * 4.将此对象作为参数传递到Thread类的构造器中，创建Thread类的对象
  * 5.通过Thread类的对象调用start()
  * @date 2022-04-04 14:42
  */
