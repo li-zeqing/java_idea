0e2ef4975eb4e6f1cb36ba75fa24e7920190e6a8
lizeqing <lizeqing77@163.com>
2022-04-08 09:54:15 +0800

    继承Thread类实现多线程同步

--- JavaSenior/multithreading/src/com/mylearn/test/WindowsTest4.java
+++ JavaSenior/multithreading/src/com/mylearn/test/WindowsTest4.java
@@ -7,15 +7,15 @@ package com.mylearn.test;
  * @date 2022-04-08 9:40
  */
 
-class Window extends Thread{
+class Window extends Thread {
     private static int ticket = 100;
 
     private static Object obj = new Object();
 
     @Override
-    public void run(){
+    public void run() {
 
-        synchronized(obj){
+        synchronized (obj) {
             while (true) {
                 if (ticket > 0) {
                     System.out.println(getName() + ":卖票，票号为：" + ticket);
